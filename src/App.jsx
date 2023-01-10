17a80575df5b6e7493e7833e8ac206b5b2d88187
Darwin Perdomo <ZeroBl@hotmail.com>
2023-01-09 20:46:23 -0400

    feat(app): Add home page to the app

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href='' className='logo'>
+          ZeroBl
+        </a>
+        <nav>
+          <a href=''>Login</a>
+          <a href=''>Register</a>
+        </nav>
+      </header>
+
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2020/03/GettyImages-1059548518.jpg?w=990&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='texts'>
+          <h2>full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className='author'>Kent C. Dodds</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className='summary'>
+            The Federal Communications Commission regulates lots of industries
+            and practices relating to telecommunications and the internet, but
+            it is now cementing its role as a space regulator by voting to
+            create a brand-new bureau specializing in the topic.
+          </p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2020/03/GettyImages-1059548518.jpg?w=990&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='texts'>
+          <h2>full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className='author'>Kent C. Dodds</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className='summary'>
+            The Federal Communications Commission regulates lots of industries
+            and practices relating to telecommunications and the internet, but
+            it is now cementing its role as a space regulator by voting to
+            create a brand-new bureau specializing in the topic.
+          </p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+          <img
+            src='https://techcrunch.com/wp-content/uploads/2020/03/GettyImages-1059548518.jpg?w=990&crop=1'
+            alt=''
+          />
+        </div>
+        <div className='texts'>
+          <h2>full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className='author'>Kent C. Dodds</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className='summary'>
+            The Federal Communications Commission regulates lots of industries
+            and practices relating to telecommunications and the internet, but
+            it is now cementing its role as a space regulator by voting to
+            create a brand-new bureau specializing in the topic.
+          </p>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
