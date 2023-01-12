4e16842433ef32a5c253815878128ca86d432f94
Darwin Perdomo <ZeroBl@hotmail.com>
2023-01-11 22:09:30 -0400

    (pages/Home): Add list of post from database data

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,24 +1,16 @@
-const Post = () => {
+const Post = ({ title, summary, cover, createdAt, author }) => {
   return (
     <div className='post'>
       <div className='image'>
-        <img
-          src='https://techcrunch.com/wp-content/uploads/2020/03/GettyImages-1059548518.jpg?w=990&crop=1'
-          alt=''
-        />
+        <img src={`http://localhost:4000/${cover}`} alt={summary} />
       </div>
       <div className='texts'>
-        <h2>full-house battery backup coming later this year</h2>
+        <h2>{title}</h2>
         <p className='info'>
-          <a className='author'>Kent C. Dodds</a>
-          <time>2023-01-06 16:45</time>
+          <a className='author'>{author.username}</a>
+          <time>{new Date(createdAt).toLocaleString()}</time>
         </p>
-        <p className='summary'>
-          The Federal Communications Commission regulates lots of industries and
-          practices relating to telecommunications and the internet, but it is
-          now cementing its role as a space regulator by voting to create a
-          brand-new bureau specializing in the topic.
-        </p>
+        <p className='summary'>{summary}</p>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react'
 import { Post } from '../components'
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/post')
+      .then((res) => res.json())
+      .then((res) => setPosts(res.posts))
+  }, [])
+
   return (
-    <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
+    <>
+      {posts.length > 0
+        ? posts.map((post) => (
+            <Post
+              key={post._id}
+              title={post.title}
+              summary={post.summary}
+              cover={post.cover}
+              createdAt={post.createdAt}
+              author={post.author}
+            />
+        ))
+        : null}
+    </>
   )
 }
 
